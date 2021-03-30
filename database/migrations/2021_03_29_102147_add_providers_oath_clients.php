f3c7eb57690be747b06ee37e5e411dba5df394b7
igomis <igomis@cipfpbatoi.es>
2021-03-30 10:41:08 +0200

    Oath actualizacion

---
+++ database/migrations/2021_03_29_102147_add_providers_oath_clients.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProvidersOathClients extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('oauth_clients', function (Blueprint $table) {
+            $table->string('provider')->after('secret')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
