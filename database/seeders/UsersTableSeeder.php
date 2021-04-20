7b69e871c8d1fc0ec6861be72be966ef6fbb43ec
igomis <igomis@cipfpbatoi.es>
2021-04-20 13:26:19 +0200

    Register and test

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(['email'=>'igomis@cipfpbatoi.es','password'=>'$2y$10$DpPd5Ioe0dnEuwQq0MnF1ONoojBGsHWDF4YO.Wly3lt08G6S.URsO'
+        ,'rol'=>'2','active'=>'1']);
+    }
+}
